e5437bf9ba95288840c31431cfdd282cc9bcaf58
Alex Spitzer <aes368@cornell.edu>
2019-10-04 11:47:15 -0400

    Add util.py

---
+++ util.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from scipy.spatial.transform import Rotation as R
+
+from geometry_msgs.msg import Vector3
+
+def tonp(obj):
+  if type(obj) == list:
+    return np.array([tonp(x) for x in obj])
+
+  w = hasattr(obj, 'w')
+  x = hasattr(obj, 'x')
+  y = hasattr(obj, 'y')
+  z = hasattr(obj, 'z')
+
+  if w and x and y and z:
+    return R.from_quat([obj.x, obj.y, obj.z, obj.w])
+
+  elif x and y and z:
+    return np.array((obj.x, obj.y, obj.z))
+
+  assert False
+
+def toros(obj):
+  if len(obj.shape) == 2:
+    if obj.shape[1] == 3:
+      return [Vector3(x[0], x[1], x[2]) for x in obj]
+
+def odomtostate(odom):
+  return np.hstack((
+    tonp(odom.pose.pose.position),
+    tonp(odom.twist.twist.linear),
+    tonp(odom.pose.pose.orientation).as_euler('ZYX')[::-1],
+    tonp(odom.twist.twist.angular)))
